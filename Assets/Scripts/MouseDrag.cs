e5879a7d0a05b8ff3033caf43afb00ba7b49d68c
Taivo <taivo@seire.ee>
2015-07-19 19:07:54 +0300

    Dummy Unity Project in 2D mode for BlueHatZach with 2 commercially free assets (puppy and moss background)

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MouseDrag : MonoBehaviour
+{
+    private Vector3 mousePosition;
+
+    void OnMouseDown()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+    void OnMouseDrag()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = Vector2.Lerp(transform.position, mousePosition, 15F);
+    }
+
+}
