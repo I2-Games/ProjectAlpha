d6122be2b2deb37fbbfd557b67a1aa954dcc2673
Jason Logan <eocnlogan@gmail.com>
2015-07-30 17:37:22 -0400

    Intro - update 2
    Figured out a workable timing system. Polished up that fader... works
    both in and out now. Need to fix an instantiative activation of the
    texture in question. Almost done. Gonna need actual artwork and finished
    audio though to finish the intro script. Thinking about making it a
    little more flowery by adding movement to the images. ^.^

--- Assets/Intro Work Folder (Jason)/Intro.js
+++ Assets/Intro Work Folder (Jason)/Intro.js
@@ -1,41 +1,27 @@
 ﻿//#pragma strict
 
-var introMusic : AudioClip;
-var introVoiceOver : AudioClip;
+var introMusic : GameObject;
+var introVoiceOver : GameObject;
 var blackScreen : Texture2D;
 var image_1 : Texture2D;
-var image_2 : Texture2D;
-var mainMenuPlaceHolder : Texture2D;
 
-var audioSource_1;
-var audioSource_2;
+
 function Start () {
-
-var loadingAudioSources = gameObject.GetComponents(AudioSource);
-audioSource_1 = loadingAudioSources[0];
-audioSource_2 = loadingAudioSources[1];
-audioSource_1.clip = introMusic;
-audioSource_2.clip = introVoiceOver;
-audioSource_2.Play();
-
-
-// There apears to be a limitation on audio scources for an object. Cashing a list and using 
-// Seperately won't work. Going to have to setup a toggle system for sound objects.
+	
+	//Use the yields to setup timing for images, fades, etc.
+	introMusic.SetActive(true);
+	yield WaitForSeconds(5);
+	introVoiceOver.SetActive(true);
+	yield WaitForSeconds(10);
+	gameObject.SetActive(false);
 
 
 }
 
-function Update () {
-
-	if ( Time.time > 2 )
-	{
-		audioSource_1.Play();
-	}
-
-}
+//Going to remove this and place it as a gui element on its own object. so that timing will be handled by the start area. and then de-active the entire intro so it won't use resources.
 
 function OnGUI () {
 
-GUI.DrawTexture( Rect(0,(Screen.height/2) - (image_1.height /4),Screen.width,image_1.height/2), image_1, ScaleMode.StretchToFill, true, 50.0f );
+	GUI.DrawTexture( Rect(0,(Screen.height/2) - (image_1.height /4),Screen.width,image_1.height/2), image_1, ScaleMode.StretchToFill, true, 50.0f );
 
 }
--- Assets/Intro Work Folder (Jason)/fadercode test.js
+++ Assets/Intro Work Folder (Jason)/fadercode test.js
@@ -1,34 +1,40 @@
 ﻿ #pragma strict
- 
- // FadeInOut
- 
+
+enum fadetype { In, Out };
+var fade : fadetype;
+var delay = 2;
+   
  var fadeTexture : Texture2D;
- var fadeSpeed = 0.01;
- var drawDepth = -1000;
- var delay = 2;
- var fading : boolean = false;
+ var fadeSpeed = 1;
+ private var drawDepth = -1000;
+ private var delayOver : boolean = false;
  
  private var alpha = 1.0; 
  private var fadeDir = -1;
  
+ function Start () {
+ 	if (fade == fadetype.Out) {
+ 		fadeDir = 1;
+ 		alpha = 0.0;
+ 	}
+ }
+ 
  function Update () {
- 	Debug.Log(Time.time);
  	if ( Time.time > delay )
  	{
- 		fading = true;
+ 		delayOver = true;
  	}
  }
  
  function OnGUI(){
      
-     if (fading){
-     alpha += fadeDir * fadeSpeed * Time.deltaTime;  
-     alpha = Mathf.Clamp01(alpha);   
-     
-     GUI.color.a = alpha;
+     if (delayOver){
+	     alpha += fadeDir * (fadeSpeed * .01) * Time.deltaTime;  
+	     alpha = Mathf.Clamp01(alpha);   
+	     GUI.color.a = alpha;
      }
      
      GUI.depth = drawDepth;
-     
      GUI.DrawTexture(Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+     
  }
