3101f68057bb988728ea141830d7763266ad2946
Jason Logan <eocnlogan@gmail.com>
2015-07-30 11:36:22 -0400

    Into - Update 1
    Got some basic placeholders, and am working up the screenfade code. It
    should be handy when we impliment the claustrophobia game mechanic, as
    well as for cutscenes. I learned that cashing multiple audio sources on
    a single object doesn't work so I'll have to split them into multiple
    objects. So far so good. Please don't mess with it anyone ok? Should be
    finished up inside the week. Gotta stop and go recruiting.

---
+++ Assets/Intro Work Folder (Jason)/Intro.js
@@ -0,0 +1,41 @@
+﻿//#pragma strict
+
+var introMusic : AudioClip;
+var introVoiceOver : AudioClip;
+var blackScreen : Texture2D;
+var image_1 : Texture2D;
+var image_2 : Texture2D;
+var mainMenuPlaceHolder : Texture2D;
+
+var audioSource_1;
+var audioSource_2;
+function Start () {
+
+var loadingAudioSources = gameObject.GetComponents(AudioSource);
+audioSource_1 = loadingAudioSources[0];
+audioSource_2 = loadingAudioSources[1];
+audioSource_1.clip = introMusic;
+audioSource_2.clip = introVoiceOver;
+audioSource_2.Play();
+
+
+// There apears to be a limitation on audio scources for an object. Cashing a list and using 
+// Seperately won't work. Going to have to setup a toggle system for sound objects.
+
+
+}
+
+function Update () {
+
+	if ( Time.time > 2 )
+	{
+		audioSource_1.Play();
+	}
+
+}
+
+function OnGUI () {
+
+GUI.DrawTexture( Rect(0,(Screen.height/2) - (image_1.height /4),Screen.width,image_1.height/2), image_1, ScaleMode.StretchToFill, true, 50.0f );
+
+}
---
+++ Assets/Intro Work Folder (Jason)/fadercode test.js
@@ -0,0 +1,34 @@
+﻿ #pragma strict
+ 
+ // FadeInOut
+ 
+ var fadeTexture : Texture2D;
+ var fadeSpeed = 0.01;
+ var drawDepth = -1000;
+ var delay = 2;
+ var fading : boolean = false;
+ 
+ private var alpha = 1.0; 
+ private var fadeDir = -1;
+ 
+ function Update () {
+ 	Debug.Log(Time.time);
+ 	if ( Time.time > delay )
+ 	{
+ 		fading = true;
+ 	}
+ }
+ 
+ function OnGUI(){
+     
+     if (fading){
+     alpha += fadeDir * fadeSpeed * Time.deltaTime;  
+     alpha = Mathf.Clamp01(alpha);   
+     
+     GUI.color.a = alpha;
+     }
+     
+     GUI.depth = drawDepth;
+     
+     GUI.DrawTexture(Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+ }
