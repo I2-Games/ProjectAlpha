b90a391821003685a550a4ee390ccba280f8da33
brandonfox <brandonfox@hotmail.co.uk>
2015-08-27 16:25:32 +0700

    Added Main Menu
    Main Menu added however save and load is unfinised

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	private bool optionsOpen = false;
+	private GameObject options;
+
+	public GameObject optionsUI;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadLevel(string levelName){
+		Application.LoadLevel(levelName);
+	}
+	public void Options(){
+		if(!optionsOpen){
+			options = Instantiate(optionsUI,Vector3.zero,Quaternion.identity) as GameObject;
+			optionsOpen = true;
+		}else{
+			Destroy(options);
+			optionsOpen = false;
+		}
+	}
+}
---
+++ Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionsScript : MonoBehaviour {
+	private float volume;
+	private Slider volumeSlider;
+
+	public bool muted = false;
+
+	// Use this for initialization
+	void Start () {
+		volumeSlider = GameObject.Find("VolumeSlider").GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnChangeVolume(){
+		if(muted){
+			AudioListener.volume = 0;
+			Debug.Log ("Muted");
+		}else{
+			volume = volumeSlider.value;
+			AudioListener.volume = volume/11;
+			Debug.Log (volume);
+		}
+	}
+	public void Mute(){
+		muted = !muted;
+		OnChangeVolume();
+	}
+	public void CloseOptions(){
+		Destroy(this.gameObject);
+	}
+}
